cc3e7dd06440a86818e23c141b87a480d0142b21
kimzeze <96248861+kimzeze@users.noreply.github.com>
2023-02-25 22:40:22 +0900

    purchageview css 일부 완성

--- src/components/PurchageView.js
+++ src/components/PurchageView.js
@@ -4,11 +4,13 @@ const PurchageView = () => {
   return (
     <div className="product">
       <img
-        src="public/assets/productImage.svg"
+        src="/assets/productImage.svg"
         alt="상품 이미지"
         className="product__img"
       />
-      <h1 className="product__title">래피젠 코로나 자가 검사 2개입X1박스</h1>
+      <h1 className="product__title">
+        래피젠 코로나 자가 검사 키트 2개입X1박스
+      </h1>
       <div className="product__price">
         <span className="">88%</span>
         <span className="">56,750원</span>
